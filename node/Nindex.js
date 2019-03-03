1d1a53cf3c51de9690e3a2bf2b3e93ccf22408c8
Trainee_Sapy27 <saptade1@in.ibm.com>
2019-03-03 20:51:12 +0530

    added nodejs files

---
+++ node/Nindex.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const parser = require('body-parser')
+const userService = require('./Nuser');
+
+app.use(parser.json())
+
+// enable CORS 
+app.use(cors())
+
+app.get('/status',(req,res) => {
+    res.json('System is up');
+}).listen(4200,()=>{
+    console.log('Server Started at 4200')
+});
+
+// fetch all users
+app.get('/Nusers',(req,res) => {
+    res.setHeader('Content-Type','application/json')
+    userService.fetchAll((data)=>{
+        res.end(JSON.stringify(data))
+    })
+});
+
+//fetch users by name
+app.get('/Nusers/findByName/:name',(req,res) => {
+    res.setHeader('Content-Type','application/json')
+    let uname = req.params.name
+    userService.fetchByName(uname,(results) =>{
+        res.json(JSON.stringify(results))
+    })
+});
+// add a new user
+app.post('/Nuser/add',(req,res)=>{
+    let userObj = req.body
+    res.setHeader('Content-Type','application/json')
+    userService.addUser(userObj,(err)=>{
+        if(err) res.statusCode(400).end('User Couldnt be Added,please try again later')
+        else res.json('User Added Successfully')
+    })
+})
+// update an existing user
+app.put('/Nuser/edit',(req,res)=>{
+    let userObj = req.body
+    res.setHeader('Content-Type','application/json')
+    userService.editUser(userObj,(err,result)=>{
+        if(err) res.statusCode(400).json('User Couldnt be Added,please try again later')
+        else{
+                res.json(JSON.stringify({ message:'User Modified' ,response : result }))
+                //res.redirect('/users')
+        }
+    })
+})
+
+app.get('/Nuser/find/id/max',(rq,rs)=>{
+    userService.getLastId((id,err)=>{
+        if(err){ 
+            console.log(err);    
+            rs.statusCode(400).json('Unable to process your request,please try again later')
+        }
+        else{
+                rs.json({maxId:id})
+        }
+    })
+})
